d3c45f1e0bdfc35d6192c4a19b2be7aef58055b4
Corey Frang <gnarf@gnarf.net>
2013-04-16 23:25:07 -0500

    a useful alias to sudo and keep my propmpt

--- init.sh
+++ init.sh
@@ -8,13 +8,20 @@ GIT_PS1_SHOWCOLORHINTS=true
 . $DOTBASE/git-prompt.sh
 
 PROMPT_COMMAND='es=$?'
-PS1="\$( if [[ \$es == 0 ]]; then echo \"\[\033[01;32m\]\342\234\223\"; else echo \"\[\033[01;31m\]\342\234\227\$es\"; fi ) \[\033[01;36m\]\$PROMPTHOST:\[\033[01;35m\]\W\[\e[0;33;49m\]\$(__git_ps1)\[\e[0;0m\]$ "
+UC="\[\033[0;36m\]"    # user's color
+[ $UID -eq "0" ] && UC="\[\033[0;31m\]" # root's color
+PS1="\$( if [[ \$es == 0 ]]; then echo \"\[\033[01;32m\]\342\234\223\"; else echo \"\[\033[01;31m\]\342\234\227\$es\"; fi ) $(echo $UC)\u@\$PROMPTHOST:\[\033[01;35m\]\W\[\e[0;33;49m\]\$(__git_ps1)\[\e[0;0m\]\\$ "
 
 gitvars() {
   export author="$(git log --format='%an <%ae>' HEAD -n1)"
   export hash="$(git log --format='%H' HEAD -n1)"
   echo "\$author=\"$author\""
   echo "\$hash=\"$hash\""
+}
+
+su() {
+  user=$( if [[ -n "$1" ]]; then echo "-u $1"; fi)
+  sudo bash --rcfile ~/.bash_profile -i $user
 }
 
 function c { pygmentize -O style=monokai -f console256 -g $1 | less -RN; }
