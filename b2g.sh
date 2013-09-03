c9aa77a83f381ce138350442613d4a14cb549671
Corey Frang <gnarf37@gmail.com>
2013-09-03 00:17:43 -0400

    Some tweaks to gitconfig and b2g that I havent committed yet

--- b2g.sh
+++ b2g.sh
@@ -18,13 +18,13 @@ gupl() {
 
 
 gaia-test-latest() {
-  /Applications/FirefoxNightly.app/Contents/MacOS/firefox -profile /Users/gnarf/Projects/gaia/profile http://test-agent.gaiamobile.org:8080 > /dev/null 2>&1
+  /Applications/FirefoxNightly.app/Contents/MacOS/firefox -profile /Users/gnarf/Projects/gaia/profile-debug http://test-agent.gaiamobile.org:8080 > /dev/null 2>&1
 }
 gaia-test() {
-  /Applications/FirefoxNightly\ 18.app/Contents/MacOS/firefox -profile /Users/gnarf/Projects/gaia/profile http://test-agent.gaiamobile.org:8080 > /dev/null 2>&1
+  /Applications/FirefoxNightly\ 18.app/Contents/MacOS/firefox -profile /Users/gnarf/Projects/gaia/profile-debug http://test-agent.gaiamobile.org:8080 > /dev/null 2>&1
 }
 gaia-b2g() {
-  /Applications/B2G.app/Contents/MacOS/b2g-bin -profile /Users/gnarf/Projects/gaia/profile http://test-agent.gaiamobile.org:8080
+  /Applications/B2G.app/Contents/MacOS/b2g-bin -profile /Users/gnarf/Projects/gaia/profile-debug --jsconsole
 }
 gaia-unit() {
   gjslint --disable 210,217,220,225 `find apps/$1 -name *.js` ; make test-agent-test APP=$1
